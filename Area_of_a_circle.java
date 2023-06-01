6f7fdf4b5d6eed77f0afabab0393011cb2a5bfb9
gowtham6752k <121657308+gowtham6752k@users.noreply.github.com>
2023-06-01 15:11:12 +0530

    import java.util.*; public class kkkk {     public static void main(String args[])     {         Scanner sc=new Scanner (System.in);         float a;         double x;         a=sc.nextFloat();         x=3.14*a*a;         System.out.format("%.2f",x);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class kkkk
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        float a;
+        double x;
+        a=sc.nextFloat();
+        x=3.14*a*a;
+        System.out.format("%.2f",x);
+    }
+}
