8b6a1446caab675c082b11901ac2ab885916ccee
gowtham6752k <121657308+gowtham6752k@users.noreply.github.com>
2023-06-01 16:08:46 +0530

    import java.util.*; public class three {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         {             int a,b,c,d;             a=sc.nextInt();             b=sc.nextInt();             c=sc.nextInt();             d=a*2*b*c*512;             System.out.println(d);         }     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class three
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        {
+            int a,b,c,d;
+            a=sc.nextInt();
+            b=sc.nextInt();
+            c=sc.nextInt();
+            d=a*2*b*c*512;
+            System.out.println(d);
+        }
+    }
+}
