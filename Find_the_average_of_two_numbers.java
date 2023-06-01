81c40562f9d6c864abcab598ff6b5e9c99ea0d4f
gowtham6752k <121657308+gowtham6752k@users.noreply.github.com>
2023-06-01 15:29:22 +0530

    import java.util.*; public class kkkk {     public static void main (String args[])     {         Scanner sc=new Scanner(System.in);         float a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=(a+b)/2;         System.out.format("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class kkkk
+{
+    public static void main (String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=(a+b)/2;
+        System.out.format("%.4f",c);
+    }
+}
